e3fc24eec2cdca081ed843d0be3c9f85129d781e
Haiikos <lukashajek9@seznam.cz>
2024-11-11 22:12:06 +0100

    vsechno pric

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from './Home';
-import About from './About';
-import Projects from './Projects';
-import Contact from './Contact';
-import Examples from './Examples';
-
-export{Home,About,Projects,Contact,Examples};
